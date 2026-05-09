450f545701e3f9972710a201c69f5491c4bb2b07
Brenda <62002152+Brenda0714@users.noreply.github.com>
2026-05-08 18:31:21 -0600

    validacion de boton  reservacion en consola y en html

--- src/app/components/reservations/reservations.ts
+++ src/app/components/reservations/reservations.ts
@@ -1,16 +1,32 @@
-import { isPlatformBrowser } from '@angular/common';
+import { DecimalPipe, isPlatformBrowser } from '@angular/common';
 import { AfterViewInit, ChangeDetectionStrategy, Component, inject, PLATFORM_ID, signal } from '@angular/core';
 import * as AOS from 'aos';
+
+interface Reserva {
+  cliente: string;
+  correo: string;
+  telefono: string;
+  fechaLlegada: string;
+  fechaSalida: string;
+  cabin:  string,
+  noches:  string,
+  precioUnitario:  string,
+  montoTotal:  string
+};
 
 
 @Component({
   selector: 'app-reservations',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './reservations.html',
   styleUrl: './reservations.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class Reservations implements AfterViewInit{
+
+
+
 private platformId = inject(PLATFORM_ID);
 
 ngAfterViewInit() {
@@ -75,5 +91,66 @@ scrollToForm() {
 }
 
 
+
+
+
+loading = signal(false);
+reservaData = signal<any>(null);
+
+iniciarPago(nombre: string, email: string, tel: string, llegada: string, salida: string, cabin: string) {
+
+
+
+
+    // 1. Validar que no haya campos vacíos
+    if (!nombre || !email || !tel || !llegada || !salida || !cabin) {
+      alert('Por favor, completa todos los campos.');
+      return;
+    }
+
+
+  const infoCabana = this.Cabins().find(c => c.title === cabin);
+  const precioNumerico = infoCabana ? infoCabana.precio.replace(',', '') : '0';
+
+
+  const fechaIn = new Date(llegada);
+  const fechaOut = new Date(salida);
+
+  // Diferencia en milisegundos dividida por milisegundos en un día
+  const diffTime = fechaOut.getTime() - fechaIn.getTime();
+  const noches = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  if (noches <= 0) {
+    alert('La fecha de salida debe ser posterior a la de llegada.');
+    return;
+  }
+  const precioPorNoche = infoCabana ? Number(infoCabana.precio.replace(',', '')) : 0;
+  const totalPagar = precioPorNoche * noches;
+
+    // 2. Creamos el objeto con la data
+    this.reservaData.set({
+      cliente: nombre,
+      correo: email,
+      telefono: tel,
+      fechaLlegada: llegada,
+      fechaSalida: salida,
+      cabin: cabin,
+      noches: noches,
+      precioUnitario: precioPorNoche,
+      montoTotal: totalPagar
+    });
+
+    // 3. Mostramos en consola para validar
+    console.log('Datos de la reservación listos para procesar:', this.reservaData());
+
+    // 4. Simulamos el proceso de pago
+    this.loading.set(true);
+
+    setTimeout(() => {
+      this.loading.set(false);
+      console.log('Simulación de pago finalizada');
+      // Aquí podrías redirigir a Stripe, PayPal o tu backend
+    }, 2000);
+  }
 
  }
