53b06be4753289226f1d39cb59b710e870fb012d
Brenda <62002152+Brenda0714@users.noreply.github.com>
2026-06-02 16:09:14 -0600

    sidebar implementacion

--- src/app/template/sidebar/sidebar.ts
+++ src/app/template/sidebar/sidebar.ts
@@ -27,4 +27,38 @@ setTimeout(() => {
   ngOnChanges() {
   this.cdr.markForCheck();
   }
+
+  isLoggedIn: boolean = false;
+  nombreUsuario: string = '';
+
+  ngOnInit() {
+  this.verificarSesion();
+}
+
+  // Revisa si hay una sesión activa guardada localmente
+verificarSesion() {
+  const userJson = localStorage.getItem('usuario'); // O como llames a tu objeto de sesión
+  if (userJson) {
+    const user = JSON.parse(userJson);
+    this.isLoggedIn = true;
+    this.nombreUsuario = user.nombre_completo || 'Huésped';
+  } else {
+    this.isLoggedIn = false;
+  }
+}
+
+// Extrae la inicial del nombre para pintarla en el círculo estético móvil
+obtenerIniciales(): string {
+  return this.nombreUsuario ? this.nombreUsuario.charAt(0).toUpperCase() : 'U';
+}
+
+// Borra los datos, cierra el menú lateral y te redirige a home
+logout() {
+  localStorage.removeItem('usuario');
+  this.isLoggedIn = false;
+  this.onClose();
+  // Aquí puedes usar tu router para mandarlo a /home si lo deseas
+}
+
+
 }
